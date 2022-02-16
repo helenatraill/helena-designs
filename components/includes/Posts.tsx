6d1713d108d6b106d01a90e3b58a7e5fb3822395
Jerome Kenway <jeromekenway@gmail.com>
2022-02-16 22:21:32 +0000

    sindoisjd

--- components/includes/Posts.tsx
+++ components/includes/Posts.tsx
@@ -37,9 +37,10 @@ export default function Posts() {
                         className={styles['archive__image']}
                         src={post?.featuredImage?.node?.sourceUrl()}
                         alt={post?.featuredImage?.node?.altText}
-                        width="100%"
-                        height={post?.featuredImage?.node?.mediaDetails?.height}
-                        objectFit="contain"
+                        //width="{post?.featuredImage?.node?.mediaDetails?.width}"
+                        //height={post?.featuredImage?.node?.mediaDetails?.height}
+                        //objectFit="cover"
+                        layout="fill"
                       />
                     </div>
                   </a>
--- pages/archive/[categorySlug]/[postSlug].tsx
+++ pages/archive/[categorySlug]/[postSlug].tsx
@@ -23,9 +23,10 @@ export default function Page() {
               <Image
                 src={post?.featuredImage?.node?.sourceUrl()}
                 alt={post?.featuredImage?.node?.altText}
-                width="100%"
-                height={post?.featuredImage?.node?.mediaDetails?.height}
-                objectFit="contain"
+                //width={post?.featuredImage?.node?.mediaDetails?.width}
+                //height={post?.featuredImage?.node?.mediaDetails?.height}
+                //objectFit="cover"
+                layout="fill"
               />
             </div>
           </div>
